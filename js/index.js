7ac946865add99e187c307c90f31606dbc4eb72f
Alexandr Morozov <moralex260@gmail.com>
2017-11-10 18:10:34 +0000

    initial

---
+++ js/index.js
@@ -0,0 +1,22 @@
+$(function(){
+    $('.buttons button').click(activatePanel);    
+});
+
+// document.querySelectorAll('.buttons button')
+//     .forEach( btn => btn.addEventListener('click', activatePanel) );
+
+function activatePanel(ev) {
+    // 1. Clear all active on buttons and panels
+    $('.buttons button, .panels div').removeClass('active');
+    // document.querySelectorAll('.buttons button, .panels div')
+    //     .forEach( el => el.classList.remove('active') ) ;
+    // 2. Set active to clicked button
+    ev.target.classList.add('active');
+    // 3. Set active on panel corresponding to button
+    document.getElementById(ev.target.dataset.panel).classList.add('active');
+}    
+ // $('.buttons button').click(function (ev){
+   //     $('.buttons button, .panels div').removeClass('active');
+  //      $(ev.target).addClass('active');
+  //      $('#'+ev.target.dataset.panel).addClass('active');
+//});    
